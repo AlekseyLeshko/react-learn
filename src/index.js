11c7f9641d54a368b99b6559f4c3b832baaa2231
AlekseyLeshko <alekseyleshko@gmail.com>
2016-11-28 15:46:24 +0400

    add shopping list

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './components/App';
+import ShoppingList from './components/ShoppingList';
 
 ReactDOM.render(
-  <App />,
+  <div>
+    <App />
+    <ShoppingList name="Aleksey" />
+  </div>,
   document.getElementById('root')
 );
