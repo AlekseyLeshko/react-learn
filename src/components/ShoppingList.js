93cb5d42d85cf474d7ef81179fa3f45cf39c9187
AlekseyLeshko <alekseyleshko@gmail.com>
2016-12-05 12:10:47 +0400

    fix

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -4,7 +4,9 @@ class ShoppingList extends React.Component {
   render() {
     return (
       <div className="shopping-list">
-        <h1>Shopping List for { this.props.name } < /h1>
+        <h1>
+          Shopping List for { this.props.name }
+        </h1>
         <ul>
           <li> Instagram </li>
           <li> WhatsApp < /li>
@@ -16,3 +18,4 @@ class ShoppingList extends React.Component {
 }
 
 export default ShoppingList;
+
